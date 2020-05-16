5f13250e0224d3c37b19db7b48eb2bd9178ac6f3
Krister Johansson <krister.johansson@spoton.se>
2020-05-16 18:32:56 +0200

    asd

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
+const data = {
+  playbackId: process.env.PLAYBACKID || 'NULL'
+}
 app.set('view engine', 'pug')
 app.set("views", path.join(__dirname, "views"));
 app.get('/', function (req, res) {
-    res.render('index')
+    res.render('index',data)
   })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
